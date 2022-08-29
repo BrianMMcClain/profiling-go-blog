c3677b94a2b82af0f4bdbe70aeced5e01d54e934
Brian McClain <brianmmcclain@gmail.com>
2022-08-29 13:40:06 -0400

    Update monte carlo and test

--- monte_carlo.go
+++ monte_carlo.go
@@ -8,11 +8,12 @@ import (
 
 func MonteCarlo(iterations int) float64 {
 	randGen := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	hitCount := 0
 
 	for i := 0; i < iterations; i++ {
-		hit := generatePoint(randGen)
+		x := randGen.Float64()
+		y := randGen.Float64()
+		hit := (math.Pow(x, 2) + math.Pow(y, 2)) < 1.0
 		if hit {
 			hitCount++
 		}
@@ -20,11 +21,4 @@ func MonteCarlo(iterations int) float64 {
 
 	return (float64(hitCount) / float64(iterations)) * 4.0
 
-}
-
-func generatePoint(randGen *rand.Rand) bool {
-	x := randGen.Float64()
-	y := randGen.Float64()
-	hit := (math.Pow(x, 2) + math.Pow(y, 2)) < 1.0
-	return hit
 }
--- monte_carlo_test.go
+++ monte_carlo_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestMonteCarlo(t *testing.T) {
-	res := MonteCarlo(400000)
+	res := MonteCarlo(1000000)
 	if res < 3.1 || res > 3.2 {
 		t.Fatalf("Inaccurate estimation of Pi: %v", res)
 	}
 }
 
 func BenchmarkMonteCarlo(b *testing.B) {
-	NUM_ITERATIONS := 10000000
+	NUM_ITERATIONS := 1
 	for n := 0; n < b.N; n++ {
 		MonteCarlo(NUM_ITERATIONS)
 	}
